35fe4b8f883a3fbf59ad67ac09f91117f87ea32e
Aliyu Yusuf <30539461+usoofali@users.noreply.github.com>
2023-05-04 05:45:43 +0100

    Add files via upload

---
+++ admin/index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Admin Login</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="form.css">
+	<style type="text/css">
+		a.hover{
+			background-color: #fff;
+		}
+	</style>
+</head>
+<body>
+
+	
+	<div class="hero">
+				<aside>
+	<a style="text-decoration: none;" href="../index.html"><button style="color: green; font-size: 30px" type="logout" name="logout" class="btn btn-success">Back to Homepage</button></a>
+</aside>
+
+		<div class="form-box">
+			  <form class="form-control">
+			  	<div class="header">
+			  		<h2 style="color: green" align="center">Select Your Department</h2>
+			  	</div>
+			  	<table border="2px">
+			  		<tr>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href="admin/index.php"><img style="width: 100px" src="admincss/logo.png" alt="computer">Computer Department</a></th>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Mathematics Department</a></th>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Geology Department&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</a></th>
+
+			  		</tr>
+			  		<tr>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Chemistry Department</a></th>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Biochemistry Department</a></th>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Plant science Department</a></th>
+			  			
+			  		</tr>
+			  		<tr>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Physics Department&nbsp&nbsp&nbsp&nbsp&nbsp</a></th>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Microbiology Department</a></th>
+			  			<th style="background-color: white"><a style="text-decoration: none;" href=""><img style="width: 100px" src="admincss/logo.png" alt="computer">Zoology Department&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp </a></th>
+			  			
+			  		</tr>
+			  	</table>
+			  </form>
+</body>
+<footer align="center">
+      
+            <div class="copyright text-center my-auto">
+                <span> <button class="btn btn-sml btn-success" type="submit">UDUS EBRS -2021</button></span>
+            </div>
+        
+        </footer>
+</html>
